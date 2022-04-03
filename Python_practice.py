8c21a071661ddfd70cb6512b1e4ca29d4eb7cdd7
Simpson944 <101375388+Simpson944@users.noreply.github.com>
2022-04-03 14:29:40 -0700

    Update

--- Python_practice.py
+++ Python_practice.py
@@ -12,9 +12,42 @@ csvpath = os.path.join('Resources','election_results.csv')
 #     File_reader= csv.reader(election_data)
 # for row in File_reader:
 #     Print(row)
-
+total_votes=0
+candidate_options=[]
+candidate_votes={}
+Winning_Candidate=""
+Winning_count=0
+Winning_Percentage=0
 election_data=open(csvpath)
 csv_reader=csv.reader(election_data)
 headers=next(csv_reader)
-# for row in csv_reader:
-print(headers)
+for row in csv_reader:
+    candidate_name=row[2]
+    if candidate_name not in candidate_options:
+        candidate_options.append(candidate_name)
+        candidate_votes [candidate_name]=0
+    candidate_votes [candidate_name]+=1
+    total_votes +=1
+for candidate_name in candidate_votes:
+    votes=candidate_votes [candidate_name]
+    vote_percentage = float(votes) / float(total_votes) * 100
+    # print(f"{candidate_name} recieved {vote_percentage}% of the Total Vote")
+    print(f"{candidate_name}:{vote_percentage:.1f}% ({votes:,})\n")  
+    if (votes > Winning_count) and (vote_percentage > Winning_Percentage):
+        Winning_count = votes
+        Winning_Percentage = vote_percentage
+        Winning_Candidate = candidate_name
+    Winning_Candidate_Summary= (
+        f"------------------------\n"
+        f'Winner: {Winning_Candidate}\n'
+        f'Winning Vote count: {Winning_count:,}\n'
+        f'Winning Percentage: {Winning_Percentage:.1f}%\n'
+        f'-------------------------\n')
+print(Winning_Candidate_Summary)      
+
+
+    
+    
+    #total_votes +=1
+#print (total_votes)
+    
